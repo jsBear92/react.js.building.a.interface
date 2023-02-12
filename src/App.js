f51ecf508dee40a95d3b848509a469d3110cc5b2
jsBear92 <js104427@gmail.com>
2023-02-12 17:20:59 +0800

    add Appointment Calendar module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BiArchive } from "react-icons/bi"
 import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment"
 
 const App = () => {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
-        <BiArchive className="inline-block text-red-400 align-top" />Hi</h1>
+      <h1 className="text-5xl mb-3">
+        <BiArchive className="inline-block text-red-400 align-top" />Your Appointment</h1>
+      <AddAppointment />
       <Search />
     </div>
   );
