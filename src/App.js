af7c662f76046520d9c90db5a7b75ef43162c937
jsBear92 <js104427@gmail.com>
2023-02-12 17:56:36 +0800

    move data.json, and add useEffect and useCallback

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import { BiArchive } from "react-icons/bi"
+import { useState, useEffect, useCallback } from "react";
+import { BiArchive } from "react-icons/bi";
 import Search from "./components/Search";
-import AddAppointment from "./components/AddAppointment"
+import AddAppointment from "./components/AddAppointment";
 import AppointmentInfo from "./components/AppointmentInfo";
-import appointmentList from "./data.json"
 
 const App = () => {
+
+  let [appointmentList, setAppointmentList] = useState([]);
+
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data => {
+        setAppointmentList(data)
+      });
+  }, [])
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData])
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
@@ -12,15 +27,14 @@ const App = () => {
       <AddAppointment />
       <Search />
 
-    <ul className="divide-y divide-gray-200">
-      {appointmentList
-      .map(appointment => (
-          <AppointmentInfo key={appointment.id}
-            appointment={appointment}
-          />
-      ))}
-    </ul>
-
+      <ul className="divide-y divide-gray-200">
+        {appointmentList
+        .map(appointment => (
+            <AppointmentInfo key={appointment.id}
+              appointment={appointment}
+            />
+        ))}
+      </ul>
     </div>
   );
 }
